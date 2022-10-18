fc35099929c8ff716cb164dfa49db751cc3d9d12
Brooke Kullberg <brookekullberg@gmail.com>
2022-10-18 09:47:13 -0700

    add triangle model and ui logic

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using System;
+using ShapeTracker.Models;
+
+namespace ShapeTracker 
+{
+
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("We'll calculate what type of triangle you have based off of the lengths of the triangle's 3 sides.");
+      Console.WriteLine("Please enter a number:");
+      string stringNumber1 = Console.ReadLine();  
+      Console.WriteLine("Enter another number:");
+      string stringNumber2 = Console.ReadLine();  
+      Console.WriteLine("Enter a third number:");
+      string stringNumber3 = Console.ReadLine();  
+      int length1 = int.Parse(stringNumber1);  
+      int length2 = int.Parse(stringNumber2);  
+      int length3 = int.Parse(stringNumber3);  
+      Triangle tri = new Triangle(length1, length2, length3);
+      string result = tri.CheckType();
+      Console.WriteLine("Your result is: " + result + ".");
+      Console.WriteLine("Would you like to check a new triangle? Please enter 'yes' or 'no'.");
+      string userResponse = Console.ReadLine(); 
+      if (userResponse == "yes" || userResponse == "Yes")
+      {
+        Main();
+      }
+    }
+  }
+}
