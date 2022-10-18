9b3d90e8bdd444923a7aa79572816139c5c683f2
Brooke Kullberg <brookekullberg@gmail.com>
2022-10-18 09:49:08 -0700

    add rectangle model and UI logic

---
+++ Models/Rectangle.cs
@@ -0,0 +1,19 @@
+namespace ShapeTracker.Models 
+{
+  public class Rectangle 
+  {
+    public int Side1 { get; }
+    public int Side2 { get; }
+
+    public Rectangle(int length1, int length2) 
+    {
+      Side1 = length1;
+      Side2 = length2;
+    }
+
+    public int GetArea() 
+    {
+      return Side1 * Side2;
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -3,11 +3,55 @@ using ShapeTracker.Models;
 
 namespace ShapeTracker 
 {
-
   class Program
   {
     static void Main()
     {
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      Console.WriteLine("Welcome to the Shape Tracker app!");
+      Console.WriteLine("You can do a few things:");
+      Console.WriteLine("- Calculate what type of triangle you have.");
+      Console.WriteLine("- Calculate the area of a rectangle.");
+      Console.WriteLine("What would you like to do? Enter 'triangle' or 'rectangle'. To exit, enter 'exit'.");
+      string userResponse = Console.ReadLine(); 
+      if (userResponse == "triangle" || userResponse == "Triangle")
+      {
+        Triangle();
+      }
+      else if (userResponse == "rectangle" || userResponse == "Rectangle")
+      {
+        Rectangle();
+      }
+    }
+
+    static void Rectangle()
+    {
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      Console.WriteLine("We'll calculate what the area of your rectangle based off of two lengths that you provide.");
+      Console.WriteLine("Please enter a number:");
+      string stringNumber1 = Console.ReadLine();  
+      Console.WriteLine("Enter another number:");
+      string stringNumber2 = Console.ReadLine();  
+      int length1 = int.Parse(stringNumber1);  
+      int length2 = int.Parse(stringNumber2);  
+      Rectangle rect = new Rectangle(length1, length2);
+      int result = rect.GetArea();
+      Console.WriteLine("The area of your rectangle is: " + result.ToString() + ".");
+      Console.WriteLine("Would you like to check the area of a new rectangle? Please enter 'yes' or 'no'.");
+      string userResponse = Console.ReadLine(); 
+      if (userResponse == "yes" || userResponse == "Yes")
+      {
+        Rectangle();
+      }
+      else
+      {
+        Main();
+      }
+    }
+
+    static void Triangle()
+    {
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
       Console.WriteLine("We'll calculate what type of triangle you have based off of the lengths of the triangle's 3 sides.");
       Console.WriteLine("Please enter a number:");
       string stringNumber1 = Console.ReadLine();  
@@ -24,6 +68,10 @@ namespace ShapeTracker
       Console.WriteLine("Would you like to check a new triangle? Please enter 'yes' or 'no'.");
       string userResponse = Console.ReadLine(); 
       if (userResponse == "yes" || userResponse == "Yes")
+      {
+        Triangle();
+      }
+      else
       {
         Main();
       }
