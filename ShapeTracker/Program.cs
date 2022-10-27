b90412236bf89c60a94efea4e30dad697fb0d3b5
Brooke Kullberg <brookekullberg@gmail.com>
2022-10-27 08:44:06 -0700

    remove rectangle

--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using ShapeTracker.Models;
 
 namespace ShapeTracker 
@@ -9,52 +10,6 @@ namespace ShapeTracker
     {
       Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
       Console.WriteLine("Welcome to the Shape Tracker app!");
-      Console.WriteLine("You can do a few things:");
-      Console.WriteLine("- Calculate what type of triangle you have.");
-      Console.WriteLine("- Calculate the area of a rectangle.");
-      Console.WriteLine("What would you like to do? Enter 'triangle' or 'rectangle'. To exit, enter any key.");
-      string userResponse = Console.ReadLine(); 
-      if (userResponse == "triangle" || userResponse == "Triangle")
-      {
-        Tri();
-      }
-      else if (userResponse == "rectangle" || userResponse == "Rectangle")
-      {
-        Rectangle();
-      }
-      else
-      {
-        Console.WriteLine("Goodbye!");
-      }
-    }
-
-    static void Rectangle()
-    {
-      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
-      Console.WriteLine("We'll calculate what the area of your rectangle based off of two lengths that you provide.");
-      Console.WriteLine("Please enter a number:");
-      string stringNumber1 = Console.ReadLine();  
-      Console.WriteLine("Enter another number:");
-      string stringNumber2 = Console.ReadLine();  
-      int length1 = int.Parse(stringNumber1);  
-      int length2 = int.Parse(stringNumber2);  
-      Rectangle rect = new Rectangle(length1, length2);
-      int result = rect.GetArea();
-      Console.WriteLine("The area of your rectangle is: " + result.ToString() + ".");
-      Console.WriteLine("Would you like to check the area of a new rectangle? Please enter 'yes' or 'no'.");
-      string userResponse = Console.ReadLine(); 
-      if (userResponse == "yes" || userResponse == "Yes")
-      {
-        Rectangle();
-      }
-      else
-      {
-        Main();
-      }
-    }
-
-    static void Tri()
-    {
       Console.WriteLine("We'll calculate what type of triangle you have based off of the lengths of the triangle's 3 sides.");
       Console.WriteLine("Please enter a number:");
       string stringNumber1 = Console.ReadLine();  
@@ -123,12 +78,14 @@ namespace ShapeTracker
     static void SeeAllTriangles()
     {
       Console.WriteLine("Here are all of your triangles:");
-      foreach (Triangle tri in Triangle.GetAll())
+      List<Triangle> allTriangles = Triangle.GetAll();
+      for (int index = 0; index < allTriangles.Count; index++)
       {
         Console.WriteLine("------------------------------------");
-        Console.WriteLine($"Side 1 has a length of {tri.Side1}.");
-        Console.WriteLine($"Side 2 has a length of {tri.Side2}.");
-        Console.WriteLine($"Side 3 has a length of {tri.Side3}.");
+        Console.WriteLine($"Triangle #{index + 1}");
+        Console.WriteLine($"Side 1 has a length of {allTriangles[index].Side1}.");
+        Console.WriteLine($"Side 2 has a length of {allTriangles[index].Side2}.");
+        Console.WriteLine($"Side 3 has a length of {allTriangles[index].Side3}.");
       }
       Console.WriteLine("------------------------------------");
       Main();
