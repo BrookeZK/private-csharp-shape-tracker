35064f18f2722e66d82b58e2e9525ab5e3189a0c
Brooke Kullberg <brookekullberg@gmail.com>
2022-10-27 09:03:01 -0700

    update welcome message

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace ShapeTracker
   {
     static void Main()
     {
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      Console.WriteLine("Welcome to the Shape Tracker app!");
       Console.WriteLine("We'll calculate what type of triangle you have based off of the lengths of the triangle's 3 sides.");
       Console.WriteLine("Please enter a number:");
       string stringNumber1 = Console.ReadLine();  
